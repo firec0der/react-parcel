a05ce4d9ad799732b13e12cf11aaa26a7290147e
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-02-26 11:50:46 +0700

    setup browser router, render "home" and "about" routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 // imports from vendors
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// imports from pages
+import { About, Home } from './pages';
 
 export default class App extends React.Component {
 
   render() {
-    return 'Hello world';
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <header>Header</header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+            </Switch>
+          </main>
+          <footer>Footer</footer>
+        </React.Fragment>
+      </BrowserRouter>
+    );
   }
 
 }
