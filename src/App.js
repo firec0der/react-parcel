50d5c49c510390bbadd3db08d565dcdbe4947f0d
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-02-28 13:46:18 +0700

    wrap the App with redux Provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 // imports from vendors
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // imports from pages
 import { About, Home } from './pages';
 
+import store from './store';
+
 export default class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <header>Header</header>
-          <main>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-            </Switch>
-          </main>
-          <footer>Footer</footer>
-        </React.Fragment>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <React.Fragment>
+            <header>Header</header>
+            <main>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+              </Switch>
+            </main>
+            <footer>Footer</footer>
+          </React.Fragment>
+        </BrowserRouter>
+      </Provider>
     );
   }
 
