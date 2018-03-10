46335778b805adf2fc83779009cdd67c5ba14bbe
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-03-10 13:17:19 +0700

    add Footer component, styles for Main layout, render content and Footer.

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,10 +1,25 @@
 // imports from vendors
 import React from 'react';
 
+// imports from components
+import { Footer } from './components';
+
+// imports from styles
+import classNames from './Main.scss';
+
 export default class MainLayout extends React.Component {
 
   render() {
-    return null;
+    const { children } = this.props;
+
+    return (
+      <div className={classNames.root}>
+        <main className={classNames.content}>
+          { children }
+        </main>
+        <Footer />
+      </div>
+    );
   }
 
 }
---
+++ src/layouts/Main/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+// imports from vendors
+import React from 'react';
+
+export default class Footer extends React.PureComponent {
+
+  render() {
+    return <footer>Footer</footer>
+  }
+
+}
