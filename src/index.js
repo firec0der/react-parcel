2b25b9ef9caeb5e9a623cc1a7c6908fbe52142d8
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-03-11 14:46:04 +0700

    add ".global." suffix to bootstrap entry, import it in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import './styles/bootstrap.global.scss';
 import './styles/main.scss';
 
 const root = document.getElementById('root');
