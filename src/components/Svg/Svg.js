60a515df54c08cf0462d4eff6161a7c0009dcda7
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-03-12 08:33:22 +0700

    add Svg pure component

---
+++ src/components/Svg/Svg.js
@@ -0,0 +1,35 @@
+// imports from vendors
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactSvgInline from 'react-svg-inline';
+import classNames from 'classnames';
+
+// imports from styles
+import styles from './Svg.scss';
+
+export default class Svg extends React.PureComponent {
+
+  static propTypes = {
+    // TODO: add propTypes from 'react-svg-inline' for correct IDE autocomplete
+    component: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.func,
+    ]),
+  };
+
+  static defaultProps = {
+    component: 'div',
+  };
+
+  render() {
+    const { className } = this.props;
+
+    const props = {
+      ...this.props,
+      className: classNames(styles.root, className)
+    };
+
+    return <ReactSvgInline {...props} />;
+  }
+
+}
