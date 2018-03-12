10cf97c5dadd465008c1154e0a85dfcb449f857f
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-03-12 12:26:07 +0700

    accept "className" prop

--- src/layouts/Main/components/Header/Header.js
+++ src/layouts/Main/components/Header/Header.js
@@ -1,5 +1,7 @@
 // imports from vendors
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 // imports from components
 import { Svg } from '../../../../components';
@@ -11,9 +13,19 @@ import GitHubSVG from '../../../../assets/github/github.svg';
 
 export default class Header extends React.PureComponent {
 
+  static propTypes = {
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    className: '',
+  };
+
   render() {
+    const { className } = this.props;
+
     return (
-      <header className={styles.root}>
+      <header className={classNames(styles.root, className)}>
         <div className="container">
           <Svg height="32px" svg={GitHubSVG} fill="white" />
         </div>
