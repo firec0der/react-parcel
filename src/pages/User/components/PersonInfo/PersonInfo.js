c50f7bfc05d276fb58d098966ebf99fa62ad1dfb
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-03-12 14:11:42 +0700

    add PersonalInfo pure component that renders avatar, full and user names

---
+++ src/pages/User/components/PersonInfo/PersonInfo.js
@@ -0,0 +1,34 @@
+// imports from vendors
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Image } from 'react-bootstrap';
+
+// imports from styles
+import styles from './PersonInfo.scss';
+
+export default class PersonInfo extends React.PureComponent {
+
+  static propTypes = {
+    avatarUrl: PropTypes.string.isRequired,
+    fullname: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  };
+
+  render() {
+    const { avatarUrl, fullname, username } = this.props;
+
+    return (
+      <div className={styles.root}>
+        <Image responsive rounded src={avatarUrl} />
+
+        <div className={styles.names}>
+          { fullname && (
+            <span className={styles.fullname}>{ fullname }</span>
+          ) }
+          <span className={styles.username}>{ username }</span>
+        </div>
+      </div>
+    )
+  }
+
+}
